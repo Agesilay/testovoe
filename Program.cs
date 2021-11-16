443bd5e5dc3821a81a6c4dd381ead7378abb458f
tester <tester@TESTER-PC>
2021-11-16 14:24:35 +0500

    MessageBox

--- Program.cs
+++ Program.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
 
 namespace testovoe
 {
     class Program
     {
-        
+        public const int N = 10;
+
         static void Main(string[] args)
         {
-            UserValidator asdf = new UserValidator();
 
-            if (asdf.IsEmailValid("adsf"))
-            {
-                Console.WriteLine("correct");
-            }
+            MessageBox.Show("Testing...");
             Console.WriteLine("Test string");
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < N; i++)
             {
                 Console.WriteLine(i);
             }
