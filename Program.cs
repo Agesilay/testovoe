680b254954bb6dc24fbb62a49b506247fb7ed51b
Пермяков Константин <2ivan_brovkin4911@mail.ru>
2021-11-16 16:34:08 +0500

    Реализован цикл вывода точек

--- Program.cs
+++ Program.cs
@@ -17,9 +17,12 @@ namespace testovoe
 
             MessageBox.Show("Testing...");
             Console.WriteLine("Test string");
-            for (int i = 0; i < N; i++)
-            {
-                Console.WriteLine(i);
+            for (int j = 1; j < N; j++){
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine();
             }
             Console.WriteLine("Hello world!");
         }
